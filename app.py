8f6541b0caea0053da65240f177706f7a9443767
Servicescentree <p20549738@gmail.com>
2025-09-08 05:09:19 +0300

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ from flask import Flask, jsonify
 # === CONFIGURATION ===
 @dataclass
 class BotConfig:
-    TOKEN: str = os.getenv('BOT_TOKEN', '8368212048:AAF094UtSmRBYB98JUtVwYHzREuVicQFIOs')
+    TOKEN: str = os.getenv('BOT_TOKEN', '8368212048:AAFPu81rvI7ISpmtixdgD1cOybAQ6T_rMjI')
     ADMIN_ID: int = int(os.getenv('ADMIN_ID', '7276479457'))
     CHANNEL_URL: str = 'https://t.me/kuznya_music'
     EXAMPLES_URL: str = 'https://t.me/kuznya_music/41'
